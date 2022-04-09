5bb3d002f56ec1bf45ecaf67058440e81f39308b
therion-666 <strike201420141@gmail.com>
2022-04-09 22:58:08 +0300

    added page + edited translate

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { Suspense } from 'react';
 import './App.css';
 import './i18n'
 import { Route, Routes } from 'react-router-dom';
-import { Main } from './components';
+import { Main, PaymentDetails } from './components';
 
 function App() {
     return (
         <Suspense fallback='Loading..'>
             <Routes>
                 <Route path='/' element={<Main />} />
+                <Route path="/payment-details" element={<PaymentDetails/>}/>
             </Routes>
         </Suspense>
     );
--- src/components/Pages/index.ts
+++ src/components/Pages/index.ts
@@ -1,1 +1,2 @@
 export { default as Main } from './Main'
+export { PaymentDetails } from './PaymentDetails'
